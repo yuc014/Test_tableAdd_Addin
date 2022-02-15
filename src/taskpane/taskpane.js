11bb89b1d86053a715e0b23390295b1985394336
Yunfan Chen <yunfanchen@microsoft.com>
2022-02-15 18:54:21 +0800

    init

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global console, document, Excel, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Excel) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+export async function run() {
+  try {
+    Excel.run(function (context) {
+      //var sheet = context.workbook.worksheets.getItem("Sample");
+      var sheet = context.workbook.worksheets.getActiveWorksheet();
+      var expensesTable = sheet.tables.add("A1:D1", true /*hasHeaders*/);
+      //expensesTable.name = "ExpensesTable";
+  
+      expensesTable.getHeaderRowRange().values = [["Date", "Merchant", "Category", "Amount"]];
+  
+      expensesTable.rows.add(null /*add rows to the end of the table*/, [
+        ["1/1/2017", "The Phone Company", "Communications", "$120"],
+        ["1/2/2017", "Northwind Electric Cars", "Transportation", "$142"],
+        ["1/5/2017", "Best For You Organics Company", "Groceries", "$27"],
+        ["1/10/2017", "Coho Vineyard", "Restaurant", "$33"],
+        ["1/11/2017", "Bellows College", "Education", "$350"],
+        ["1/15/2017", "Trey Research", "Other", "$135"],
+        ["1/15/2017", "Best For You Organics Company", "Groceries", "$97"]
+      ]);
+  
+      // sheet.activate();
+  
+      return context.sync();
+    })
+    .catch(function (error) {
+      console.log("Error: " + error);
+      if (error instanceof OfficeExtension.Error) {
+        console.log("Debug info: " + JSON.stringify(error.debugInfo));
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
